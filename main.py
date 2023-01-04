0555065e16e05a95ece9f7c3a664a8679b3af88f
Technopy311 <xmenmagnet@gmail.com>
2023-01-04 16:50:09 -0300

    Added the app idea and description

---
+++ main.py
@@ -0,0 +1,19 @@
+"""
+
+Create a tracker for expenses, 
+for that i need to get user input for each expense
+where the user can input:
+    1- Expense Name.
+    2- Amount.
+    3- Description.
+    4- Date.
+
+After the user inputs expenses, the app shows all the expenses,
+the view should be in a table format and, and at the last row, 
+show the total amount of all the expenses.
+
+
+This app will be CLi based, and then made GUI with Tkinter
+
+
+"""
