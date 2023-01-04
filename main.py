db10ba4bc0f5eba175027caf7f6b26be13f4c133
Technopy311 <xmenmagnet@gmail.com>
2023-01-04 17:03:43 -0300

    Created basic function skeleton

--- main.py
+++ main.py
@@ -15,5 +15,31 @@ show the total amount of all the expenses.
 
 This app will be CLi based, and then made GUI with Tkinter
 
+To save the expenses,  will use a simple txt file as DB.
+and each line will contain 1 expense
+
+Basically I must create a CLi CRUD for the app.
 
 """
+
+from datetime import datetime
+
+
+def main():
+    print("Hi")
+
+def create():
+    pass
+
+def read():
+    pass
+
+def update():
+    pass
+
+def delete():
+    pass
+
+
+if __name__ == "__main__":
+    main()
