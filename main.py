6f9cd0c0d19b9fb4b7795476062175afbba37bdf
Technopy311 <xmenmagnet@gmail.com>
2023-01-04 23:23:59 -0300

    Added first two Crud Methods

--- main.py
+++ main.py
@@ -7,20 +7,45 @@ logging.basicConfig(format='%(levelname)s- %(asctime)s - %(message)s', level=log
 
 path = f"{getcwd()}\\"
 
-def create():
-    pass
+def addexpense(database):
+    # logging.debug("Add expense function called")
+    print("\n### Añadir un gasto ###")
+
+    expense = input("Ingrese el nombre del gasto: ")
+    amount = input("Ingrese la cantidad gastada (sin puntos o comas): ")
+    description = input("Escriba una pequeña descripción: ")
+
+    with open (f"{path}{database}", 'a') as db:
+        logging.debug(f"Opened DB: {database}")
+        db.write(f"\n{expense};{amount};{description}")
+        # logging.debug(f"Wrote in the DB: {expense};{amount};{description}")
 
 
-def read():
-    pass
+def showexpense(database):
+    # logging.debug("Show expenses function called")
+    print("\n### Mostrar los Gastos ###\n")
+    print("#  ID    Nombre    Cantidad    Descripción\n")
+    sum = 0
+
+    with open (f"{path}{database}", "r") as db:
+        for index, line in enumerate(db):
+            if index == 0:
+                header = line.split("#")
+                # logging.debug(f"Structure: {line.split('#')}")
+                #print(f"## {header[0]} ## {header[1]} ## {header[2]} ##")
+            elif line == "" or line =="\n":
+                print("  No hay gastos registrados")
+            else:
+                parsed = line.split(";")
+                # logging.debug(f"parsed line: {parsed}")
+                print(f"  {index}  {parsed[0]}  {parsed[1]}  {parsed[2]}")
+                sum += int(parsed[1])
+
+        print(f"\nTotal de Gastos: {sum}\n\n")
 
 
-def update():
-    pass
-
-
-def delete():
-    pass
+def deleteexpense(database):
+    logging.debug("Delete expense function called")
 
 
 def checkDb(path):
@@ -44,11 +69,11 @@ def createDb():
     file = f"{path}{dbname}.etdb"
     
     with open(file, "x") as database:
-        database.write(f"# nombre: {dbname} ")
-        database.write(f"# descripción: {dbdescription} ")
-        database.write(f"# fecha de creación: {datetime.now()} ")
+        database.write(f"Nombre: {dbname} ")
+        database.write(f"#Descripción: {dbdescription} ")
+        database.write(f"#fecha de creación: {datetime.now()} ")
 
-    print(f"Base de datos creada exitosamente, guardada en: {file}")
+    print(f"Base de datos creada exitosamente, guardada en: {file}\n\n")
 
 
 def availabledbs():
@@ -61,7 +86,8 @@ def availabledbs():
 
 
 def main():
-    print("\n#### Bienvenido al contador de Gastos ####")
+    print("\n#### Bienvenido al contador de Gastos ####\n\n")
+
     logging.debug("App Path: " + path)
 
     existing = checkDb(path)
@@ -75,12 +101,22 @@ def main():
     print("### Base de Datos disponibles ###\n")
     availabledbs()
 
-    userdb = input("## Escriba el nombre de la base de datos a utilizar ##\nej: testing.edtb\nBASE DE DATOS: ")
-
+    userdb = input("\n### Escriba el nombre de la base de datos a utilizar ###\nEj: testing.etdb\n\nBASE DE DATOS: ")
+    print("\n ### Menu de Usuario ### \n")
     while True:
-        pass
-        
+        option = input("Opciones:\n + Añadir Gasto (1).    + Ver los gastos (2).\n + Eliminar Gasto(3). \n\nIngrese Nro de opción: ")
 
+        if int(option) == 1:
+            addexpense(userdb)
+        elif int(option) == 2:
+            showexpense(userdb)
+        elif int(option) == 3:
+            deleteexpense(userdb)
+        else:
+            logging.debug(f"Option not recognized: {option}")
+            pass
+
+        
 
 if __name__ == "__main__":
     try:
